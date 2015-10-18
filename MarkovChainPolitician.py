b3ab2f55087c52e1cb68d7d8d0c7a381c335b83b
Mevan Babakar <mevanbabakar@gmail.com>
2015-10-18 18:38:54 +0100

    Made the API doc into a function, which is pulled into MCP.py. Now asks for raw input for politician name

--- MarkovChainPolitician.py
+++ MarkovChainPolitician.py
@@ -1,28 +1,10 @@
+from TheyWorkForYouAPI import get_speeches
+#import TheyWorkForYouAPI - if you import this way you would use sentence = TheyWorkForYouAPI.get_speeches
+
 # 1. Be able to open a text file, and split it into a list of words.   e.g.["The", "quick", "brown", "fox", "jumped", "over" ...]
 
-sentence = """I said on the steps of Downing Street on May 8 that this would be a 'one nation' government, with working people at its heart. In its simplest terms, that means if you want to work hard and get on in life, this government will be on your side.
-
-Whoever you are, wherever you live, whatever your background, whatever stage of life you are at, I believe this government can help you fulfil your aspirations. And let me be clear, when I say whoever you are, I mean it.
-
-Whether you voted for me or campaigned against me; whether you are middle income or low paid or not in work at all, whether you live in a leafy suburb or an inner-city community, this government wants to extend opportunity, and what in the election campaign I called a good life, for all. That for me is the 'one nation' ideal.
-
-We have taken great strides towards this in the past 5 years. Two million more people in work. Wages rising. More children in good or outstanding schools than ever before. And yet, for all the progress we have made, there are still far too many people in our country unable to reach their potential.
-
-When less than 1 in 10 children in residential care get 5 good GCSEs and when more than 1.5 million children are still living in households where no-one works, I won't sit back and think the job is done. Far from it. I want everyone in this country to have the opportunity to get on and make a good life for themselves. That is the task for the next 5 years.
-
-And today, I want to set out how we will achieve it.
-
-Long-term economic plan
-It starts, quite simply, by continuing to work through our long-term economic plan. Line one, rule one, of any plan to extend opportunity is to ensure a strong economy. And a secure and strong economy means getting the deficit down.
-
-I know there are those who think that this is somehow not progressive. But let me tell you this. There's nothing progressive about asking the next generation to pay off the debts we couldn't be bothered to deal with; nothing progressive about robbing from our children. There's nothing progressive about paying more in debt interest than we spend on our schools.
-
-Make no mistake, if we're running unsustainable deficits, and interest rates are going through the roof it's working people and children in the poorest families who will suffer the most. If taxes are high, and businesses are firing rather than hiring, make no mistake, it's working people and their children who will lose out first.
-
-So yes, we will finish the job of the turning the economy around and doing what I call the bread and butter of good government, competently running the economy, offering people the security they need to get on in their lives.
-
-And that is the first step in improving the life chances of everyone in our country."""
-
+print "Which politician would you like to search for?"
+sentence = get_speeches(raw_input())
 
 words = sentence.split()
 
@@ -52,7 +34,7 @@ import random
 make_sentence = random.choice(dictionary.keys()) # to start off the sentence we randomly pick a key
 list_sentence = list(make_sentence) # makes these a list so things can be appended
 
-for i in range(15): #range here defines how long the sentence is 
+for i in range(20): #range here defines how long the sentence is 
 	list_sentence.append(random.choice(dictionary[list_sentence[-2], list_sentence[-1]]))
-print list_sentence 
+print " ".join(list_sentence)
 
